bb005e4ddb6f02ea636bc50416f3dfa2bd08c655
Sidnei da Motta Silva <sidneim@hotmail.com>
2019-02-04 23:52:43 -0200

    Correção - Temporizador , exiber em tempo real, o número de movimentos que cada jogador fez. Reponsividade para dispositivos móveis

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 # Memory Game Project
 
+Projeto Jogo da Memória.
+
+O jogo apresenta 16 cartas colocadas aleatoriamente em uma grade, cartas com simbolos diversos duplicados (diamante, avião de papel, ancora, raio, cubo ,folha, bicicleta, bomba ). 
+
+## Instructions
+Instruções
+
+Clone o repositório do GitHub 
+$ git clone https://github.com/sidneimtt/Project-MemoryGame-Sidnei.git
+
+- Carregar o arquivo index.html.
+
+- Cada clique do mouse em uma carta a mesma é virada e apresenta a parte oculta da carta. 
+- Clicar em uma primeira carta deve fazê-la virar, apresentando o símbolo e a mesma permanecer virada.
+- Clicar em uma segunda carta diferente também deve fazê-la virar e apresentar o símbolo.
+- Se o simbolo da primeira carta e da segunda carta combinarem eles permanecem virados apresentando o simbolo das duas cartas.
+- Se o simbolo da primeira carta e da segunda carta não combinarem os simbolos das cartas são escondidos novamente.
+- O jogo atribui estrelas com base no número de movimentos do jogador, iniciando com 3 estrelas e quando o número de movimentos atinge 18 é removido 1 estrela do placar ( 22 movimentos é removido 2 estrelas, 29 movimentos é removido 3 estrelas)
+- Quando o jogador conseguir combinar todas as cartas é apresentada uma mensagem 
+de fim de jogo com número de movimentos, estrelas e o tempo.
+- Duas opções para reiniciar o jogo são apresentadas. 
+
 ## Table of Contents
 
 * [Instructions](#instructions)
 * [Contributing](#contributing)
 
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
--- js/app.js
+++ js/app.js
@@ -1,25 +1,28 @@
 /*
-   ( var array ) Cria e inicializa a lista de cartas.
-   ( var array1 ) Cria variavel lista para 2 cartas abertas.
+   ( var cards ) Cria e inicializa a lista de cartas.
+   ( var endTime, startTime ) Cria e inicializa variavel de time.
+   ( var openCards ) Cria variavel lista para 2 cartas abertas.
    ( var valDeck ) Cria variavel que recebe o elemento do DOM de Id. "#deck*" para atribuição do event Click.
    ( var moveCounter ) Cria variavel para controlar contador de movimentos e inicializa.
-   ( var matchCar ) Cria variavel para controlar contador de cartas que combinam.	 
+   ( var matchCar ) Cria variavel para controlar contador de cartas que combinam.
+   ( var stars1 ) Cria e inicializa variavel para controle de quantidade de estrelas.
+   ( var operationTook ) Cria e inicializa variavel de tempo de operaçao.	 
 */
-
-var array = ["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt","fa fa-cube","fa fa-anchor","fa fa-leaf",
-"fa fa-bicycle","fa fa-diamond","fa fa-bomb","fa fa-leaf","fa fa-bomb","fa fa-bolt","fa fa-bicycle","fa fa-paper-plane-o","fa fa-cube"];
-var array1 = [];
+var cards = ["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt","fa fa-cube","fa fa-leaf","fa fa-bicycle","fa fa-bomb"];
+var openCards = [];
 var valDeck;
 var moveCounter = 0;
 var matchCar = 0;
+var stars1 = 3;
 var endTime, startTime = new Date();
 var operationTook = 0;
+stopWatch();
+limpaLista();
 
 
 /*
 ( Função shuffle )  Embaralha a lista de cartas e retorna uma nova lista para array.
 */
-
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
@@ -35,18 +38,23 @@ function shuffle(array) {
 
 /*
 ( Função limpaLista ) Utilizada para restart do jogo.
-( array1 ,moveCounter, matchCar ) Inicializa as variaveis lista de cartas abertas e contadores de movimento.
+( openCards ,moveCounter, matchCar ) Inicializa as variaveis lista de cartas abertas e contadores de movimento.
 Remove as cartas da lista e oculta os símbolo da carta.
 Apresenta o valor de quantidade de movimentos.
+Chama a função stopWatch().
 Chama a função novaLista().
 */
-
 function limpaLista() {
-  	array1 = [];
+  	openCards = [];
 	matchCar = 0;
+	moveCounter = 0;
+	stars1 = 3
 	$('.card').removeClass("card match open show").addClass("card");
   	startTime = new Date();
+  	$('.moves').html( "<em>" + moveCounter + "  MOVES  " +  "</em>" );
+	stopWatch();
   	novaLista();
+return;  	
 }
 
 
@@ -54,11 +62,10 @@ function limpaLista() {
 ( Função novaLista )  Chama função shuffle que embaralha a lista de cartas e retorna um nova lista.
 Altera a localização das cartas no DOM ('li') com base na nova lista criada.
 */
-
 function novaLista() {
+	var array = cards.concat(cards);
 	array = shuffle(array);
 	$('.fa-star').remove();
-	moveCounter = 0;
 	var star = $('.stars');
 	var currLi = $('li i');
 		for (var y = 0; y < array.length; y++) {
@@ -67,13 +74,13 @@ function novaLista() {
 		for ( var i = 0; i < 3; i++) {
 				star.append('<li><i class="fa fa-star"></i></li>');
 		}
+return;		
 }
-	
 
+	
 /*
 configurar o ouvinte de eventos para cada carta com base no Id. Se uma carta for clicada:
 */
-
 $("#deck1").on('click', function() {
 	valDeck = $("#deck1");
 	displayCard(valDeck);
@@ -161,17 +168,16 @@ $("#deck16").on('click', function() {
  Quando 2 cartas na lista, chama a função matchCard() para verificar se combinam.
  Quando 3 cartas na lista, remove da lista 2 cartas ( index 0 e 1), esconde o simbolo da carta.
  */
-
 function displayCard(valDe) {
    	valDe.addClass('open show');
-   	array1.push(valDe);  // adinciona no fim da lista 
-	if ( array1.length === 2 ) {
+   	openCards.push(valDe);  // adinciona no fim da lista 
+	if ( openCards.length === 2 ) {
         matchCards();		
-	} else if ( array1.length === 3 ) { 
-			array1[0].toggleClass('open show');
-      		array1[1].toggleClass('open show');	
-      		array1.shift();  // remove da lista
-      		array1.shift();  
+	} else if ( openCards.length === 3 ) { 
+			openCards[0].toggleClass('open show');
+      		openCards[1].toggleClass('open show');	
+      		openCards.shift();  // remove da lista
+      		openCards.shift();  
       		checkMoveCounter();
 	}
 return;
@@ -181,16 +187,15 @@ return;
 /*
  ( Função matchCards ) Se as cartas coincidirem, trava as cartas na posição aberta.
 */
-
 function matchCards() {
-	var card1 = array1[0].children().first();
-	var card2 = array1[1].children().first();
+	var card1 = openCards[0].children().first();
+	var card2 = openCards[1].children().first();
 	var cla1 = card1[0].classList
 	var cla2 = card2[0].classList
 	if ( cla1[1] === cla2[1] ) {
     	matchCar++;
-    	array1[0].toggleClass('match open show');
-    	array1[1].toggleClass('match open show');
+    	openCards[0].toggleClass('match open show');
+    	openCards[1].toggleClass('match open show');
     	checkMoveCounter();
     } 
 return; 
@@ -201,34 +206,47 @@ return;
  Incrementar o contador de movimentos e exibi na página
  E verifica se todos os cartões combinam.    
  */
-
 function checkMoveCounter() {
 	moveCounter++;
-	if ( matchCar === 8 ) {
-		$('.moves').html( moveCounter );
-		$('#Moves').text(' MOVES -  GAME OVER ');
-		endTime = new Date();
-		operationTook =  (endTime.getMinutes() - startTime.getMinutes());
+	$('.moves').html( "<em>" + moveCounter + "  MOVES  " +  "</em>" );
+		if ( matchCar === 8 ) {
+				$('.moves').html( "<em>" + moveCounter + "  MOVES  " +  "</em>" );
+				endTime = new Date();
+				operationTook =  (endTime.getMinutes() - startTime.getMinutes());
+				scoreStar();
+				finaliza();
+		} else {
 		scoreStar();
-		return;
-	} 
+	    }	 
+return;
 }
 
 
 /*
- Incrementar o contador de estrelas e exibi na modal com resultado final. 
- Se todos os cartões tiverem correspondido, exibe uma mensagem com a pontuação final.    
- Mensagem de encerramento.
+ Incrementar o contador de estrelas e exibi durante o jogo e no modal. 
 */
 function scoreStar() {
-	var stars1 = 0;
-	if ( moveCounter <= 18 ) { 
-		stars1 = 5; /// 5 estrelas
-	} else if ( moveCounter <= 24 ) {
-		stars1 = 4; 
-	} else if ( moveCounter > 24 ) {
-		stars1 = 3; /// 3 estrelas
-	}	
+	if ( moveCounter === 18 ) { 
+		stars1 = 2; // 2 estrelas
+		$('.fa-star').remove();
+		$('.stars').append('<li><i class="fa fa-star"></i></li>');
+		$('.stars').append('<li><i class="fa fa-star"></i></li>');
+	} else if ( moveCounter === 22 ) {
+		stars1 = 1; // 1 estrela
+		$('.fa-star').remove();
+		$('.stars').append('<li><i class="fa fa-star"></i></li>');
+	} else if ( moveCounter === 29 ) {
+		stars1 = 0; // 0 estrelas
+		$('.fa-star').remove();
+	}
+return;	
+}
+
+
+/* Se todos os cartões tiverem correspondido, exibe uma mensagem com a pontuação final no modal com resultado final.    
+ Mensagem de encerramento.
+ */
+function finaliza() {		
 	$( "div.modal-body" ).html(function() {
   		var emphasis = "<em> With " + moveCounter + " Moves and " + stars1 + " Stars." + "</em>";
   		return "<p class='fa fa-star fa-3x'>" + emphasis + "</p>";
@@ -239,3 +257,21 @@ return;
 }
 
 
+// Apresenta relogio temporizador
+function stopWatch() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  $("#stopWatch").html( h + ":" + m + ":" + s );
+  var t = setTimeout(stopWatch, 500);
+}
+
+
+// Adiciona zero em frente para numero < 10 
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};   
+    return i;
+}
